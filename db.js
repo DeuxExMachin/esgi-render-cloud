84bc6f53b170ea347d81da40c19e44c0c2631bdf
Jason Kinet <jason_kinet@outlook.com>
2023-09-26 14:52:07 +0200

    add database config

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'cloud_3snm', // Database name
+  'clouduser', // User
+  'H1MEeybLiegNFioQqpZw4QUkUtqm4mIF', // Password
   {
-    host: '', // Host
+    host: '@dpg-ck9d0i1ehpqs73c3ip4g-a', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
